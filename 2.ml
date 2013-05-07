fa797bb2fde1f350c55aaddf5511bcc6321d793f
ldz <ldz@ldz.ca>
2013-05-07 19:24:36 -0400

    Section 2.2
    - transliterated examples, experimented with OUnit
    - no exercises, yet...

--- 2.ml
+++ 2.ml
@@ -45,3 +45,78 @@ let rec update = function
 let rec suffixes = function
     [] -> [[]]
   | (x :: xs) -> (x :: xs) :: (suffixes xs)
+
+module type SET = sig
+  type elem
+  type set
+  val empty : set
+  val insert : elem * set -> set
+  val member : elem * set -> bool
+end
+
+
+module type ORDERED = sig
+  type t
+  val eq : t * t -> bool
+  val lt : t * t -> bool
+  val leq : t * t -> bool
+end
+
+module UnbalancedSet (Element : ORDERED) : SET
+    with type elem = Element.t =
+struct
+  type elem = Element.t
+  type tree = E | T of tree * elem * tree
+  type set = tree
+
+  let empty = E
+
+  let rec member = function
+    (x, E) -> false
+  | (x, T(a, y, b)) ->
+    if Element.lt (x, y) then member (x, a)
+    else if not (Element.leq (x, y)) then member (x, b)
+    else true
+
+  let rec insert = function
+    (x, E) -> T (E, x, E)
+  | (x, (T (a, y, b) as s)) ->
+    if Element.lt (x, y) then T (insert (x, a), y, b)
+    else if not (Element.leq (x, y)) then T (a, y, insert (x, b))
+    else s
+end
+
+module OrderedInt : ORDERED 
+    with type t = int =
+struct
+  type t = int
+  let eq (a, b) = a == b
+  let lt (a, b) = a < b
+  let leq (a, b) = a <= b
+end
+
+module Set = UnbalancedSet (OrderedInt)
+
+let rec set2list = function
+  [] -> Set.empty
+  | (x::xs) -> Set.insert (x, set2list xs)
+
+open OUnit
+
+let set = set2list [3;1;5;2;8;9;6;7]
+
+let test_set_member _ =
+  assert_equal true (Set.member (3, set));
+  assert_equal true (Set.member (1, set));
+  assert_equal true (Set.member (5, set));
+  assert_equal true (Set.member (2, set));
+  assert_equal true (Set.member (8, set));
+  assert_equal true (Set.member (9, set));
+  assert_equal true (Set.member (6, set));
+  assert_equal true (Set.member (7, set));
+  assert_equal false (Set.member (11, set));
+  assert_equal false (Set.member (0, set))
+
+let suite = "IntSet" >::: ["test_set_member" >:: test_set_member]
+
+let _ = run_test_tt_main suite
